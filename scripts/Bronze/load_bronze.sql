0d38f583cee6f351edc6d88b2c285460c46db6d4
UmairAbbas <146115199+umairkhan1998@users.noreply.github.com>
2025-03-10 13:04:17 +0500

    Update load_bronze.sql

--- scripts/Bronze/load_bronze.sql
+++ scripts/Bronze/load_bronze.sql
@@ -1,9 +1,16 @@
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
+DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME; 
+BEGIN TRY
 
+SET @batch_start_time = GETDATE();
 PRINT '*******************';
 PRINT 'Loading Bronze Layer CRM';
 PRINT '*******************';
+
+
+PRINT 'Loading the crm_cust_info';
+SET @start_time = GETDATE();
 
 TRUNCATE TABLE bronze.crm_cust_info;
 
@@ -14,7 +21,13 @@ WITH(
 	FIELDTERMINATOR =',',
 	TABLOCK
 );
+SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
 
+
+SET @start_time = GETDATE();
+PRINT 'Loading the crm_prd_info';
 TRUNCATE TABLE bronze.crm_prd_info;
 
 BULK INSERT bronze.crm_prd_info
@@ -25,9 +38,13 @@ WITH(
     FIELDTERMINATOR = ',',
     TABLOCK
 );
+SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
 
 
-
+ SET @start_time = GETDATE();
+PRINT 'Loading the crm_sales_details';
 TRUNCATE TABLE bronze.crm_sales_details;
 
 BULK INSERT bronze.crm_sales_details
@@ -38,12 +55,18 @@ WITH(
     FIELDTERMINATOR = ',',
     TABLOCK
 );
+SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
+
 
 
 
 PRINT '==================';
 PRINT 'Loading Bronze Layer ERP';
 PRINT '==================';
+SET @start_time = GETDATE();
+PRINT 'Loading the erp_cust_az12';
 TRUNCATE TABLE bronze.erp_cust_az12;
 BULK INSERT bronze.erp_cust_az12
 FROM 'C:\Users\umair\OneDrive\Desktop\warhouse\datasets\source_erp\CUST_AZ12.csv'
@@ -52,8 +75,13 @@ WITH(
 FIRSTROW = 2,
 FIELDTERMINATOR = ',',
 TABLOCK);
+SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
 
 
+SET @start_time = GETDATE();
+PRINT 'Loading the erp_loc_a101';
 TRUNCATE TABLE bronze.erp_loc_a101;
 
 BULK INSERT bronze.erp_loc_a101
@@ -63,6 +91,13 @@ WITH(
 FIRSTROW = 2,
 FIELDTERMINATOR = ',',
 TABLOCK);
+SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
+
+
+SET @start_time = GETDATE();
+PRINT 'Loading the erp_px_cat_g1v2';
 
 TRUNCATE TABLE bronze.erp_px_cat_g1v2;
 
@@ -72,6 +107,18 @@ WITH(
 
 FIRSTROW = 2,
 FIELDTERMINATOR = ',',
-TABLOCK);
+TABLOCK
+);
+SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
 
+
+
+END TRY
+  BEGIN CATCH
+  PRINT '======================';
+  PRINT 'ERROR';
+  PRINT '======================';
+  END CATCH
 END
